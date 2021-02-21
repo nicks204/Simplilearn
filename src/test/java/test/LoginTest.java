9bd445d36be553bc8f645d3e6d041f98a089c8fd
nicks204 <59753817+nicks204@users.noreply.github.com>
2021-02-21 21:42:36 +0530

    Update LoginTest.java

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -29,9 +29,9 @@ public class LoginTest {
 	@BeforeMethod
 	public void setup() {
 		
-		System.setProperty("webdriver.chrome.driver", "/home/nikunjshah204gm/Downloads/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		
-		System.setProperty("webdriver.gecko.driver", "/home/nikunjshah204gm/Downloads/geckodriver");
+		System.setProperty("webdriver.gecko.driver", "geckodriver");
 		
 		driver = new ChromeDriver();
 		
